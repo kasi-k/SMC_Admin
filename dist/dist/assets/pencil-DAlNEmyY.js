1534389485efbd51353ad19b26ba1f5d4db4d3b1
kasi-k <kasivishwanath698@gmail.com>
2025-02-26 13:23:33 +0530

    distcommit

---
+++ dist/dist/assets/pencil-DAlNEmyY.js
@@ -0,0 +1,1 @@
+const s="/assets/pencil-2ujEHNqU.png";export{s as E};
--- src/Host.jsx
+++ src/Host.jsx
@@ -1,4 +1,4 @@
-export const API="http://localhost:5000"
+export const API="https://smc-mvc.onrender.com"
 export function formatDate(dateString) {
     const date = new Date(dateString);
   
